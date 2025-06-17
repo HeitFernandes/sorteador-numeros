b75a5c410064db1ade109628f30d1c22212944c7
Heitor <fernandesheitort2@gmail.com>
2025-06-17 17:03:59 -0300

    adiciona função numero aleatório

--- app.js
+++ app.js
@@ -1,12 +1,24 @@
  function sortear (){
 
-  /* Criando a função para sortear pegando os elementos pelo document.getElementById */
-    let quantidade = document.getElementById('quantidade').value;
-    let de = document.getElementById('de').value;
-    let ate = document.getElementById('ate').value;
+    /* Criando a função para sortear pegando os elementos pelo document.getElementById */
 
-    alert(`quantidade: ${quantidade}`);
-    alert(`Do Numero: ${de}`);
-    alert(`ate: ${ate}`);
-    
+    let quantidade = parseInt(document.getElementById('quantidade').value);
+    let de = parseInt(document.getElementById('de').value);
+    let ate = parseInt(document.getElementById('ate').value);
+    /* array para pegar os numeros sorteados */
+    let sorteados = [];
+    let numero;
+    /* loop para a quantidade de numeros a serem sorteados */
+    for(let i = 0; i < quantidade; i++){
+        numero = obterNumeroAleatorio(de, ate);
+        sorteados.push(numero);
+    }
+
+    alert(sorteados);
+        
+ }
+
+ function obterNumeroAleatorio(min, max){
+    /* Implementando a função para gerar o numero aleatorio entre de e ate */
+    return Math.floor(Math.random() * (max - min + 1)) + min;
  }
