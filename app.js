ddf86c344b45a98039a8de0a118ba61d0a59855d
Heitor Fernandes <fernandesheitort2@gmail.com>
2025-06-17 16:08:28 -0300

    Adiciona Comentário

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
  function sortear (){
 
+  /* Criando a função para sortear pegando os elementos pelo document.getElementById */
     let quantidade = document.getElementById('quantidade').value;
     let de = document.getElementById('de').value;
     let ate = document.getElementById('ate').value;
