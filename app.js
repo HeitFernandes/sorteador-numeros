e6d2fea73e02cfb7a253f4a910b4886bf197a0c2
Heitor <fernandesheitort2@gmail.com>
2025-06-19 13:29:21 -0300

    Implementação Função Reiniciar Botão

--- app.js
+++ app.js
@@ -11,14 +11,32 @@
     /* loop para a quantidade de numeros a serem sorteados */
     for(let i = 0; i < quantidade; i++){
         numero = obterNumeroAleatorio(de, ate);
+
+        while(sorteados.includes(numero)){
+         numero = obterNumeroAleatorio(de, ate);
+      }
         sorteados.push(numero);
     }
 
-    alert(sorteados);
-        
+    let resultado = document.getElementById('resultado');
+    resultado.innerHTML = `<label class="texto__paragrafo">Números sorteados: ${sorteados}</label>`;
+   alterarStatusBotão();        
  }
 
  function obterNumeroAleatorio(min, max){
     /* Implementando a função para gerar o numero aleatorio entre de e ate */
     return Math.floor(Math.random() * (max - min + 1)) + min;
+ }
+
+ function alterarStatusBotão(){
+   let botao = document.getElementById('btn-reiniciar');
+   if(botao.classList.contains('container__botao-desabilitado')){
+
+      botao.classList.remove('container__botao-desabilitado');
+      botao.classList.add('container__botao');
+
+   }else {
+      botao.classList.remove('container__botao');
+      botao.classList.add('container__botao-desabilitado');
+   }
  }
